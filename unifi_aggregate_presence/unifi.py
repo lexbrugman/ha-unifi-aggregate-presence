7b23d3a15c6e450f709766ceee887a6ad9d24b4f
Lex Brugman <lexbrugman@users.noreply.github.com>
2021-07-01 12:47:39 +0200

    Add request timeout for unifi controller

--- unifi_aggregate_presence/unifi.py
+++ unifi_aggregate_presence/unifi.py
@@ -1,10 +1,29 @@
+import logging
+import requests
 from pyunifi.controller import Controller as BaseUnifiClient
+
+_LOGGER = logging.getLogger(__name__)
+DEFAULT_TIMEOUT = 8
+
+
+class DefaultTimeoutHTTPAdapter(requests.adapters.HTTPAdapter):
+    def send(self, *args, **kwargs):
+        if kwargs["timeout"] is None:
+            kwargs["timeout"] = DEFAULT_TIMEOUT
+
+        return super().send(*args, **kwargs)
 
 
 class UnifiClient(BaseUnifiClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.session.mount("http://", DefaultTimeoutHTTPAdapter())
+        self.session.mount("https://", DefaultTimeoutHTTPAdapter())
+
     def _logout(self):
         try:
-            super(UnifiClient, self)._logout()
+            super()._logout()
         except ValueError:
             pass
 
